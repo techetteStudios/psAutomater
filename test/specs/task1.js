f3cecaf1053c65fb4ca6046c0e8189f3ac72929b
Techette Studios <christine@techetteStudios.com>
2017-12-01 17:08:02 -0500

    Added waitForExist after login/logout

--- test/specs/task1.js
+++ test/specs/task1.js
@@ -2,16 +2,16 @@ var chai = require('chai');
 var assert = require('assert');
 
 describe('form authentication page', function() {
-  console.log("|==========[TASK_1]==========|");
+  console.log("|==========[TASK_1: FORM AUTHENTICATION]==========|");
   it('should be able to open web page', function () {
-    console.log("==========[TEST 1]==========");
+    console.log("==========[__1__TEST 1]==========");
     browser.url('http://the-internet.herokuapp.com/login');
     var url = browser.getUrl();
     assert.equal(url, 'http://the-internet.herokuapp.com/login',  'Not on correct url');
     console.log("[1] Opened correct web page");
   });
   it('should submit login form', function () {
-    console.log("==========[TEST 2]==========");
+    console.log("==========[__1__TEST 2]==========");
     var username = $('#username');
     var password = $('#password');
 
@@ -25,23 +25,28 @@ describe('form authentication page', function() {
     console.log("[2.C] Clicked Login");
   });
   it('should successfully login', function () {
-    console.log("==========[TEST 3]==========");
-    var loginButton = $('.radius')
+    console.log("==========[__1__TEST 3]==========");
+    var loginButton = $('.radius');
     loginButton.click();
+    var logoutButton = $('.button')
+    logoutButton.waitForExist(5000);
     var url = browser.getUrl();
     assert.equal(url, 'http://the-internet.herokuapp.com/secure',  'Login Page URL was not correct');
     console.log("[3] Successfully Logged In");
   });
   it('should click logout', function() {
-    console.log("==========[TEST 4]==========");
+    console.log("==========[__1__TEST 4]==========");
     var logoutButton = $('.button')
     var response = logoutButton.click();
     assert.equal(response.state, 'success', 'Logout Button Was Not Successfully Clicked');
     console.log("[4] Clicked Logout");
   });
   it('should successfully logout', function(){
-    console.log("==========[TEST 5]==========");
+    console.log("==========[__1__TEST 5]==========");
+    var username = $('#username');
+    username.waitForExist(5000);
     var url = browser.getUrl();
+    console.log(url);
     assert.equal(url, 'http://the-internet.herokuapp.com/login', 'Logout Page URL was not correct');
     console.log("[5] Successfully Logged Out");
   });
